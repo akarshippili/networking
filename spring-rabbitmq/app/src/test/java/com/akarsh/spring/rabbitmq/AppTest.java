618d2c12b806ae194a024da22ebb6ca675967e77
IPPILIAKARSH <ippiliakarsh@gmail.com>
2023-01-16 02:08:36 +0530

    update: setup

--- spring-rabbitmq/app/src/test/java/com/akarsh/spring/rabbitmq/AppTest.java
+++ spring-rabbitmq/app/src/test/java/com/akarsh/spring/rabbitmq/AppTest.java
@@ -4,11 +4,12 @@
 package com.akarsh.spring.rabbitmq;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+@SpringBootTest
+public class AppTest {
+    @Test
+    public void contextLoads() {}
 }
