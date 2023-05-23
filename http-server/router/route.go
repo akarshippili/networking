3264471d745cf0a6ad467fa99a61dcf020184f1b
IPPILIAKARSH <ippiliakarsh@gmail.com>
2023-05-23 23:25:58 +0530

    added getTitle Function

--- http-server/router/route.go
+++ http-server/router/route.go
@@ -1,14 +1,26 @@
 package router
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
+	"regexp"
 
 	"github.com/akarshippili/networking/http-server/fs"
 )
 
 var templates = template.Must(template.ParseFiles("./templates/view.html", "./templates/edit.html"))
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	path := r.URL.Path
+	exp := regexp.MustCompile("^/(edit|view|save)/([A-Za-z0-9]+)$")
+	if !exp.MatchString(path) {
+		return "", errors.New("invalid page title")
+	}
+
+	return path[len("/view/"):], nil
+}
 
 func renderTemplate(w http.ResponseWriter, tmpl string, page *fs.Page) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", page)
@@ -18,23 +30,28 @@ func renderTemplate(w http.ResponseWriter, tmpl string, page *fs.Page) {
 }
 
 func ViewHandler(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	fileName := path[len("/view/"):]
+	fileName, err := getTitle(w, r)
 	fmt.Printf("User trying access %s\n", fileName)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
 
 	page, err := fs.Load(fileName)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+fileName, http.StatusFound)
 		return
 	}
-
 	renderTemplate(w, "view", page)
 }
 
 func EditHandler(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	fileName := path[len("/edit/"):]
+	fileName, err := getTitle(w, r)
 	fmt.Printf("User trying access %s\n", fileName)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
 
 	page, err := fs.Load(fileName)
 	if err != nil {
@@ -47,9 +64,13 @@ func EditHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func SaveHandler(w http.ResponseWriter, r *http.Request) {
-	fileName := r.URL.Path[len("/save/"):]
-	body := r.FormValue("body")
+	fileName, err := getTitle(w, r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
 
+	body := r.FormValue("body")
 	page := fs.Page{
 		Title: fileName,
 		Body:  []byte(body),
