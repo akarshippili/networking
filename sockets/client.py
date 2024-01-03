c1f08b8ec1b685997b0cab5d25d447ac936bc8fe
IPPILIAKARSH <ippiliakarsh@gmail.com>
2024-01-04 03:57:45 +0530

    feat: added buffering and handling multiple conn concurrently

--- sockets/client.py
+++ sockets/client.py
@@ -4,14 +4,34 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-final_msg = ""
+
+buffer_size = 16
+HEADER_LENGHT = 10
 
 while True:
-    msgBytes = s.recv(1)
-    msg = msgBytes.decode("utf-8")
     
-    if(len(msg) <= 0): break
-    final_msg += msg
-
-print(final_msg)
+    first_msg = True
+    msg_length = None
+    msg = ""
+    
+    while True:
+        sub_msg_bytes = s.recv(buffer_size)
+        sub_msg = sub_msg_bytes.decode("utf-8")
+        
+        if(first_msg):
+            
+            if(len(sub_msg) == 0): 
+                s.close()
+                exit(0)
+                
+            first_msg = False
+            msg_length = int(sub_msg[:HEADER_LENGHT])
+            msg += sub_msg[HEADER_LENGHT:]
+        else:  
+            msg += sub_msg
+        
+        if(len(msg) == msg_length):
+            print(f"Message: {msg}, Lenght: {msg_length}.")
+            final_msg, msg_length, msg = True, None, ""
+            break
 
--- sockets/server.py
+++ sockets/server.py
@@ -1,14 +1,39 @@
 import socket
+import time
+import threading
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-s.listen(5)
+HEADER_LENGHT = 10
+def getSocketMsg(msg):  return f"{len(msg):<{HEADER_LENGHT}}{msg}"
 
+def serve(sock, addr, fn):
+    print(f"[thread-id: {threading.get_native_id()}] connection established from {addr}")    
+    try:
+        msg = getSocketMsg("Hey!!!, welcome to the server!!!")
+        sock.send(bytes(msg, 'utf-8'))
+        
+        count = 10
+        while(count):
+            time.sleep(1)
+            msg = getSocketMsg(str(fn()))
+            sock.send(bytes(msg, 'utf-8'))
+            count -= 1 
+    except Exception as ex:
+        print(ex)
+    finally:
+        sock.close()
+    
+def listenAndServe():
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((socket.gethostname(), 1234))
+            s.listen(5)
+            while True:
+                clientSocket, clientAddress = s.accept()
+                threading.Thread(target=serve, args=(clientSocket, clientAddress, time.time)).start()
+    except Exception as ex:
+        print(ex)
+    finally:
+        s.close()
 
-
-while True:
-    clientSocket, clientAddress = s.accept()
-    print(f"connection established from {clientAddress}")
-    clientSocket.send(bytes("Hey!!!, welcome to the server!!!", 'utf-8'))
-    clientSocket.close()
+listenAndServe()
